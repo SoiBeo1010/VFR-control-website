72dae3fe55adad28712a22a5be2e144b9b82059d
SoiBeo1010 <gtuan87@gmail.com>
2026-07-12 01:26:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1729,3 +1729,5 @@ app.mount("/", StaticFiles(directory="public", html=True), name="static")
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
 
+
+
